16c2524e4eb20b4728dfd21ca5f61c40b25264cb
cyrus01337 <cyrus21337@gmail.com>
2023-02-05 21:27:14 +0000

    Ext and logging refactor, event HMR POC

--- src/bot.py
+++ src/bot.py
@@ -1,27 +1,27 @@
-"""
-So I want to create a system that enables both team members to run their own commands with whatever prefix they choose,
-the prefix aspect is already setup, though the problem here is actually being able to differentiate between the 2 users
-initially. There's a way to grab the ID of the application owner so maybe looking there would be better to look into.
-"""
-import asyncio
-import contextlib
+from __future__ import annotations
+
 import os
 import pathlib
 import logging
+import typing
 
 import discord
 from discord.ext import commands
-from dotenv import load_dotenv
+
+if typing.TYPE_CHECKING:
+    from typing import Any, Callable, Coroutine
 
 log = logging.getLogger('bot')
 
 
-class TestingBot(commands.Bot):
-    def __init__(self) -> None:
+class Bot(commands.Bot):
+    def __init__(self):
         super().__init__(
-            command_prefix="?",
+            command_prefix=os.environ["PREFIX"],
             intents=discord.Intents.all(),
         )
+
+        self.overridden_on_message: Callable[[Bot, discord.Message], Coroutine[Any, Any, None]] = None
 
     async def load_extension(self, extension: str):
         try:
@@ -37,24 +37,22 @@ class TestingBot(commands.Bot):
         except Exception as error:
             log.error(f'Failed to unload extension {extension!r}', exc_info=error)
 
-    async def setup_hook(self) -> None:
+    async def setup_hook(self):
         await self.load_extension("jishaku")
 
-        for file in pathlib.Path('exts').glob('**/*.py'):
+        for file in pathlib.Path('src/exts').glob('**/*.py'):
             *tree, _ = file.parts
             ext = f"{'.'.join(tree)}.{file.stem}"
 
             await self.load_extension(ext)
 
+    async def on_message(self, message: discord.Message):
+        if self.overridden_on_message:
+            bot = self
 
-async def main(token: str):
-    async with TestingBot() as bot:
-        discord.utils.setup_logging()
-        await bot.start(token)
+            try:
+                return await self.overridden_on_message(bot, message)
+            except Exception as error:
+                log.error('Failed to process overridden on_message', exc_info=error)
 
-
-if __name__ == '__main__':
-    load_dotenv()
-
-    with contextlib.suppress(KeyboardInterrupt):
-        asyncio.run(main(os.environ['TOKEN']))
+        await self.process_commands(message)
