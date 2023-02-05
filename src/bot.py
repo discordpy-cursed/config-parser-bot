650747529a0e14bdd145c8db6809d3793e78cceb
cyrus01337 <cyrus21337@gmail.com>
2023-02-05 19:52:32 +0000

    Embed logging into extension management

--- src/bot.py
+++ src/bot.py
@@ -1,3 +1,8 @@
+"""
+So I want to create a system that enables both team members to run their own commands with whatever prefix they choose,
+the prefix aspect is already setup, though the problem here is actually being able to differentiate between the 2 users
+initially. There's a way to grab the ID of the application owner so maybe looking there would be better to look into.
+"""
 import asyncio
 import contextlib
 import os
@@ -7,7 +12,6 @@ import logging
 import discord
 from discord.ext import commands
 from dotenv import load_dotenv
-
 
 log = logging.getLogger('bot')
 
@@ -19,18 +23,29 @@ class TestingBot(commands.Bot):
             intents=discord.Intents.all(),
         )
 
+    async def load_extension(self, extension: str):
+        try:
+            await super().load_extension(extension)
+            log.info(f'Loaded extension {extension!r}')
+        except Exception as error:
+            log.error(f'Failed to load extension {extension!r}', exc_info=error)
+
+    async def unload_extension(self, extension: str):
+        try:
+            await super().unload_extension(extension)
+            log.info(f'Unloaded extension {extension!r}')
+        except Exception as error:
+            log.error(f'Failed to unload extension {extension!r}', exc_info=error)
+
     async def setup_hook(self) -> None:
         for file in pathlib.Path('exts').glob('**/*.py'):
             *tree, _ = file.parts
             ext = f"{'.'.join(tree)}.{file.stem}"
-            try:
-                await self.load_extension(ext)
-                log.info(f'Loaded extension {ext!r}')
-            except Exception as e:
-                log.error(f'Failed to load extension {ext!r}', exc_info=e)
+
+            await self.load_extension(ext)
 
 
-async def start(token: str):
+async def main(token: str):
     async with TestingBot() as bot:
         discord.utils.setup_logging()
         await bot.start(token)
@@ -40,4 +55,4 @@ if __name__ == '__main__':
     load_dotenv()
 
     with contextlib.suppress(KeyboardInterrupt):
-        asyncio.run(start(os.environ['TOKEN']))
+        asyncio.run(main(os.environ['TOKEN']))
