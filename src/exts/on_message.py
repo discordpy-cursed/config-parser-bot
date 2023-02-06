5300aa73f6b6eb8f705f45cad15d02b640cf1a95
cyrus01337 <cyrus21337@gmail.com>
2023-02-06 22:34:19 +0000

    Patch bot typing

--- src/exts/on_message.py
+++ src/exts/on_message.py
@@ -13,13 +13,12 @@ async def on_message(self: Bot, message: discord.Message):
     await self.process_commands(message)
 
 
-# TODO: replace bot type with custom bot type via type_checking
 # TODO: refactor so that `overridden_on_message` is not externally depended on
 async def setup(bot: Bot):
-    bot.overridden_on_message = on_message
+    bot.on_message = on_message
 
 
 async def teardown(bot: Bot):
     disable_overridden_on_message = None
 
-    bot.overridden_on_message = disable_overridden_on_message
+    bot.on_message = disable_overridden_on_message
