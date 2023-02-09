c6662c55ea26ee4d8b2e593a1e4048359aa8235b
cyrus01337 <cyrus21337@gmail.com>
2023-02-09 22:39:03 +0000

    Add HMR support for prefix

--- src/exts/prefix.py
+++ src/exts/prefix.py
@@ -2,19 +2,22 @@ from __future__ import annotations
 
 import os
 import typing
+from dotenv import load_dotenv
+
+from src.constants import STATIC_FALLBACK_PREFIX
 
 if typing.TYPE_CHECKING:
     from src.bot import Bot
 
-STATIC_FALLBACK_PREFIX = "?"
 
-
-async def command_prefix(*_) -> str | tuple[str]:
+async def command_prefix(*_) -> str:
     return os.environ.get("PREFIX", STATIC_FALLBACK_PREFIX)
 
 
 async def setup(bot: Bot):
     bot.command_prefix = command_prefix
+
+    load_dotenv(override=True)
 
 
 async def teardown(bot: Bot):
