3c852865039d52c900d9dd18b3188f2647d2995f
cyrus01337 <cyrus21337@gmail.com>
2023-02-09 19:03:08 +0000

    Remove inner monologue

--- src/exts/events/on_reaction_add.py
+++ src/exts/events/on_reaction_add.py
@@ -1,14 +1,3 @@
-"""
-on_reaction_add gets added with a name like so:
-
-async def setup(bot: Bot):
-    bot.add_event("on_reaction_add", balls)
-
-When the extension is reloaded, something in the events system causes
-the callback to be registered as a function for the on_<function_name>
-event, when it should be registered as a function for the
-on_reaction_add event
-"""
 from __future__ import annotations
 
 import typing
