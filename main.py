def16def619579f9b88a2514d0eee6d74149de2e
cyrus01337 <cyrus21337@gmail.com>
2023-02-09 03:02:37 +0000

    Gracefully handle loss of connection

--- main.py
+++ main.py
@@ -5,17 +5,20 @@ import os
 import discord
 from dotenv import load_dotenv
 
-from src.bot import Bot
+from src import Bot
+from src import config
 
 
 async def main(token: str):
     async with Bot() as bot:
         discord.utils.setup_logging()
+
         await bot.start(token)
 
 
 if __name__ == '__main__':
     load_dotenv()
+    config.assert_prerequisites()
 
-    with contextlib.suppress(KeyboardInterrupt):
+    with contextlib.suppress(KeyboardInterrupt, asyncio.CancelledError):
         asyncio.run(main(os.environ['TOKEN']))
