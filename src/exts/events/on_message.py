aa641bfa8d32b33191375b951e9b791e7853659c
cyrus01337 <cyrus21337@gmail.com>
2023-03-31 15:24:18 +0100

    Spell-check

--- src/exts/events/on_message.py
+++ src/exts/events/on_message.py
@@ -12,7 +12,7 @@ async def on_message(bot: Bot, message: discord.Message):
     await bot.process_commands(message)
 
 
-# TODO: abstract setups and teardowns when missing
+# TODO: abstract setups and tear-downs when missing
 # TODO: prefer decorators over explicit calls to add_x methods
 async def setup(bot: Bot):
     bot.add_event(on_message)
